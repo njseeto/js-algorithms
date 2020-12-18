32dbb7b1e1d515d3d2f8299fcc303a2bc918dca3
Natalie Seeto <njseeto@users.noreply.github.com>
2020-12-18 17:04:21 +0000

    Primality Test
    The Primality Test determines whether a number (n) is a prime number.
    The algorithm divides the numbers that are less than n.
    If there is a number that is divisible by n then we return false,
    meaning the input number is not a prime number.
    
    If there is a number that is only divisible by 1 then we return true,
    meaning the input number is a prime number.
    
    Best case: n = 1 OR n = 2. We have O(1) Constant Time
    Average case: This is when n is an average number. eg. n = 8. We get O(n), Linear Time
    Worst case: This is when n is a very large prime number,
    eg: n = 27, 277. We have O(n) Linear Time

--- fibonacci.js
+++ fibonacci.js
@@ -1,9 +1,6 @@
 // Return the nth element of the Fibonacci sequence eg: fib(4) = 5
 
 const fibonacci = (n) => {
-	// calculate the element up until n
-	// return n
-
 	const arr = [1,1]
 	for(i = 2; i < n+1; i++){
 		arr.push(arr[i - 2] + arr[i - 1])
@@ -12,3 +9,5 @@ const fibonacci = (n) => {
 }
 
 module.exports = fibonacci
+
+// This function yields a Linear Time Complexity and is the best solution for this problem
---
+++ primality.js
@@ -0,0 +1,10 @@
+const isPrime = (n) => {
+	for(i = 2; i < n; i++){
+		if(n % i === 0){
+			return false
+		}
+	}
+	return true
+}
+
+module.exports = isPrime
