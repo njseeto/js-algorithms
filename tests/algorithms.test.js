0358121079434b613ab57c10aa2b1e3e23ca53aa
Natalie <njseeto@users.noreply.github.com>
2020-12-20 12:00:45 +0000

    Organise tests
    Move tests into one file and add `describe` blocks to help organise code.

---
+++ tests/algorithms.test.js
@@ -0,0 +1,34 @@
+const func = require('../sum-numbers')
+const isPrime = require('../primality')
+const fibonacci = require('../fibonacci')
+
+describe('Sums all numbers in an array', () => {
+  test('adds 1 + 3 + 10 to equal 14', () => {
+    expect(func.sumNumbers([1, 3, 10])).toBe(14)
+  })
+
+  test('adds 1 + 2 + 3 + 4 + 5 to equal 15', () => {
+    expect(func.sumNumbersReduce([1, 2, 3, 4, 5])).toBe(15)
+  })
+})
+
+describe('Fibonacci algorithm', () => {
+  test('The 4th element should equal 5', () => {
+    expect(fibonacci(4)).toBe(5)
+  })
+
+  test('The 5th element should equal 8', () => {
+    expect(fibonacci(5)).toBe(8)
+  })
+
+})
+
+describe('Primality Test', () => {
+  test('9 should return false', () => {
+    expect(isPrime(9)).toBeFalsy()
+  })
+
+  test('7 should return true', () => {
+    expect(isPrime(7)).toBeTruthy()
+  })
+})
--- tests/primality.test.js
+++
@@ -1,9 +0,0 @@
-const isPrime = require('../primality')
-
-test('9 should return false', () => {
-  expect(isPrime(9)).toBeFalsy()
-})
-
-test('7 should return true', () => {
-  expect(isPrime(7)).toBeTruthy()
-})
