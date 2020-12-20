d8653df4592861ff3eafa815624cba04e7c2f605
Natalie <njseeto@users.noreply.github.com>
2020-12-20 12:00:45 +0000

    Organise tests
    Move tests into one file and add `describe` blocks to help organise code.

--- tests/fibonacci.test.js
+++
@@ -1,9 +0,0 @@
-const fibonacci = require('../fibonacci')
-
-test('The 4th element should equal 5', () => {
-  expect(fibonacci(4)).toBe(5)
-})
-
-test('The 5th element should equal 8', () => {
-  expect(fibonacci(5)).toBe(8)
-})
