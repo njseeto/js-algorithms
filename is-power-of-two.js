a3667df49c8df6f431ba191f8c9a56f32d76c9a5
Natalie Seeto <njseeto@users.noreply.github.com>
2020-12-24 17:06:31 +0000

    Algorithms: Power of Two Optimised
    This solution uses the bitwise operator `&`
    This solution compares the binary of the numbers to determine of `n` is a power of two.
    Time Complexity: O(1), Constant Time.
    
    Explanation:
    When a number is a power of two they only every have one bit, eg:
    2 => 0010
    4 => 0100
    8 => 1000
    ...etc
    
    To compare numbers in binary, we check the places, if there is a 0 present that amounts to a 0. Eg:
    4 => 0100
    8 => 1000
    => 0000
    So here `4 & 8` equals 0.
    
    If we compare `4 & 7`, we get 4.
    4 => 0100
    7 => 0111
    => 0100
    
    If we use the new formula, `n & (n-1)` we will always get 0 for a number that is a power of two. Eg:
    `4 & 3`, equals 0.
    4 => 0100
    3 => 0011
    => 0000

--- is-power-of-two.js
+++ is-power-of-two.js
@@ -4,19 +4,9 @@ const isPowerOfTwo = (n) => {
   if(n < 1){
     return false
   }
- let dividedNumber = n
-  while(dividedNumber !== 1){
-    if(dividedNumber % 2 !== 0){
-      return false
-    }
-    dividedNumber = dividedNumber / 2
-  }
-  return true
-}
+  return (n & (n - 1)) === 0
+ }
 
 module.exports = isPowerOfTwo
 
-// Time complexity: O(log n), Logarithmic.
-
-// Best case: n = 3; Linear is the best case.
-// Worst case: n = 1125899906842624 (n is a large number that is a power of two), Logarithmic is the worst case.
+// Time complexity: O(1), Constant Time.
