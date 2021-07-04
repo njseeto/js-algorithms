43c9847eca07ba4433a67a24160e86a01506e3ef
Natalie <njseeto@users.noreply.github.com>
2021-07-04 18:28:30 +0100

    Linear Search for Objects
    This function allows for linear search of objects.
    
    It takes in a comparator function, which makes the function more dynamic.
    The comparator function allows you to pass in the key, in this case 'name'.
    This means we would need to know the key we want to pass in and compare.
    
    The comparator function finds the key and compares the values of the keys,

--- tests/search-algorithms.test.js
+++ tests/search-algorithms.test.js
@@ -1,6 +1,11 @@
 const findElement = require("../search-algorithms/linear-search")
+const findObj = require("../search-algorithms/linear-search-objects")
 
 let arr = [5, -3, 10, -1, 12, 6]
+let ObjArr = [
+  {name: 'nat', age: 32},
+  {name: 'bear', age: 23}
+]
 
 describe('Finds the element in an array', () => {
   test('finds the element index', () => {
@@ -13,3 +18,27 @@ describe('Does not find element in the array', () => {
     expect(findElement(arr, 8)).toBe('Element not found')
   })
 })
+
+describe('Finds the object', () => {
+  test('finds object index', () => {
+    expect(findObj(
+      ObjArr,
+      {name: 'bear', age: 23},
+      function(el, it){
+        return el.name === it.name
+      }
+      )).toBe(1)
+    })
+  })
+
+describe('Does not find the object', () => {
+  test('shows correct message', () => {
+    expect(findObj(
+      ObjArr,
+      {name: 'who', age: 21},
+      function(el, it){
+        return el.name === it.name
+      }
+      )).toBe('Object not found')
+    })
+  })
