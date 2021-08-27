3a8f5c644b38a840d03ee6aa41b09d3f5e0a330f
Natalie Seeto <njseeto@users.noreply.github.com>
2021-08-27 15:59:59 +0100

    Quicksort
    This algorithm is divides and conquers.
    Here we divide the array into smaller chunks and sort them recursively.
    This array uses a pivot element and partitions the array into two sub-arrays,
    one sub-array holds the elements that are smaller than the pivot and
    another sub-array holds the elements that are larger than the pivot.
    We then take each sub-array and sort those (recursively).
    
    Time complexity: Best case and average case, the arrays are not sorted. O(n * log n)
    Worst case, the arrays are sorted. O(n^2)
    
    The reason the worst case is when the array is already sorted is because we do not take advantage
    of the splitting into sub-arrays.

--- tests/sorting-algorithm.test.js
+++ tests/sorting-algorithm.test.js
@@ -1,9 +1,17 @@
 const bubbleSort = require('../sorting-algorithms/bubble-sort')
+const quicksort = require('../sorting-algorithms/quicksort')
 
 let arr = [3, 12, 8, 54, 32, 11]
+let sortedArr = [3, 8, 11, 12, 32, 54]
 
 describe('Bubble sort', () => {
   test('sorts the array in ascending order', () => {
-    expect(bubbleSort(arr)).toEqual([3, 8, 11, 12, 32, 54])
+    expect(bubbleSort(arr)).toEqual(sortedArr)
+  })
+})
+
+describe('Quicksort', () => {
+  test('sorts the array in ascending order', () => {
+    expect(quicksort(arr)).toEqual(sortedArr)
   })
 })
