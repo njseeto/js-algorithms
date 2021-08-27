141d2a2465fdb21f48e883a4fc4521bc75115856
Natalie Seeto <njseeto@users.noreply.github.com>
2021-08-27 17:18:38 +0100

    Merge sort
    Splits the array into multiple sub-arrays that conisist of no more than 2 elements.
    These sub-arrays are sorted and then merged back together.
    
    Time complexity: The algorithm performs the same regardless of the array it is given.
    O(n * log n)
    
    Note: The merge array might seem like the best option in terms of time complexity, which
    is true when dealing with a large array.
    However, since we need to keep track of multiple sub-arrays, this algorithm may take
    up more memory than other sorting algorithms.

--- tests/sorting-algorithm.test.js
+++ tests/sorting-algorithm.test.js
@@ -1,5 +1,6 @@
 const bubbleSort = require('../sorting-algorithms/bubble-sort')
 const quicksort = require('../sorting-algorithms/quicksort')
+const mergeSort = require('../sorting-algorithms/merge-sort')
 
 let arr = [3, 12, 8, 54, 32, 11]
 let sortedArr = [3, 8, 11, 12, 32, 54]
@@ -13,5 +14,11 @@ describe('Bubble sort', () => {
 describe('Quicksort', () => {
   test('sorts the array in ascending order', () => {
     expect(quicksort(arr)).toEqual(sortedArr)
+  })
+})
+
+describe('Merge sort', () => {
+  test('sorts array in ascending order', () => {
+    expect(mergeSort(arr)).toEqual(sortedArr)
   })
 })
